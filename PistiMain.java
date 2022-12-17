4990f3c7e85b040d82ebfa29fb2865c85b47e07e
denizyts <denizyetis56@gmail.com>
2022-12-17 21:27:25 +0300

    Almost done, game works pretty well
    -game done, playable
    -computer can move the card which has the same value with top of the board.
    
    to do:
    score calculation.
    high score list.

--- PistiMain.java
+++ PistiMain.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Random;
 
 public class PistiMain {
     public static void main(String[] args) { 
-
 
     Card[] deck = new Card[52];
     deck = deckcreate();  
@@ -84,7 +85,7 @@ public static Card[] deckcreate(){      //A function creates a array of card obj
 
     }
 
-public static void view(Card[] deck){
+public static void view(Card[] deck){           //shows current board :)
 
 
 System.out.println("____________________________________________________________");
@@ -123,19 +124,252 @@ System.out.println("____________________________________________________________
 
 }
 
+public static Card[] choise(Card[] deck){        //a func allows you choose your card for throw :)
+
+Scanner scn = new Scanner(System.in);
+
+int chs = 1;                       //chs can be 4 maximum.
+int indicate = 0;                  //helps throwing card.
+String jack = "J";
+String top = "";
+boolean upisti = false;        //util for pisti
+int score = 0;                //just for pisti, if pisti thing happened score increase 10. 
+int jackhelp = 0;                  //helps for when we used jack, jack should not be on top !!
+int qcob = 0;                  //quantity of cards on board, helps pisti stuff.
+
+System.out.println("Choose a card for throw");
+
+for(int i=0; i<52; i++){
+if( deck[i].location == 2){
+
+    System.out.println("Enter " + chs + " for throw " + deck[i].value() + deck[i].suit());
+chs++;
+}
+}
+chs--;
+
+try{
+    int rc = scn.nextInt();
+
+    if(rc < 1){ 
+        System.out.println("ARE YOU KIDDING ME ?");
+        return choise(deck);
+    }
+
+    if(chs < 2 && rc > 1){
+        System.out.println("ARE YOU KIDDING ME ?");
+        return choise(deck);
+    }
+    if(chs < 3 && rc > 2){
+        System.out.println("ARE YOU KIDDING ME ?");
+        return choise(deck);
+    }
+    if(chs < 4 && rc > 3){
+        System.out.println("ARE YOU KIDDING ME ?");
+        return choise(deck);
+    }
+    if(chs < 5 && rc > 4){
+        System.out.println("ARE YOU KIDDING ME ?");
+        return choise(deck);
+    }
+    else{
+    for(int i=0; i<52; i++){
+        if( deck[i].location == 2){
+            indicate++;
+         if(rc == indicate){
+
+            for(int j = 0; j<52 ; j++){
+                if(deck[j].location == 2004){          //top represents top cards value.
+                    top = deck[j].value();
+                }
+            }
+
+            for(int j = 0; j<52 ; j++){
+                if(deck[j].location == 1) { 
+                   qcob++;
+                }
+            }
+
+
+           if(deck[i].value().equals(jack)){         //takes all cards !!!
+
+            System.out.println("jack takes all :)");
+               jackhelp++;
+                 deck[i].assignLocation(4);
+
+            for(int j = 0; j<52 ; j++){
+
+                if(deck[j].location == 2004){
+                    deck[j].assignLocation(4);
+                }
+            
+                if(deck[j].location == 1){
+                     deck[j].assignLocation(4);
+                }
+            }
+           }
+
+
+            if(deck[i].value().equals(top) && qcob == 0){          //pisti thing.
+                System.out.println("PISTI !!!!!!! ");
+
+                for(int j = 0; j<52 ; j++){
+
+                    if(deck[j].location() == 1){
+                        deck[j].assignLocation(4);
+                    }
+                
+                    if(deck[j].location == 2004){
+                        deck[j].assignLocation(4);
+                    }
+            }
+            score += 10;
+            upisti = true;
+        }
+
+
+            else if(deck[i].value().equals(top) && upisti == false){
+
+                System.out.println("you got it");
+
+                deck[i].assignLocation(4);
+
+                for(int j = 0; j<52 ; j++){
+
+                    if(deck[j].location == 2004){
+                        deck[j].assignLocation(4);
+                    }
+                
+                    if(deck[j].location == 1){
+                         deck[j].assignLocation(4);
+                    }
+    
+                }
+            }
+
+            else if(deck[i].value() != top && jackhelp == 0){
+
+                for(int j=0; j<52 ; j++){
+                    if(deck[j].location == 2004){
+                        deck[j].assignLocation(1);
+                    }
+                }
+                deck[i].assignLocation(2004);
+            }
+
+        }
+        }
+        }
+       upisti = false;
+        }
+        
+indicate = 0;
+top = "";
+}
+catch(InputMismatchException e){
+    System.out.println("ENTER A INTEGER !!!!!!");
+    return choise(deck);
+}
+catch(NoSuchElementException e){
+    System.out.println("ENTER A INTEGER !!!!!!");
+    return choise(deck);
+}
+finally{
+
+}
+
+return deck;
+
+}
+
+public static Card[] ComputerTurn(Card[] deck){     //this func contains, computers moves !!! 
+
+    String top = "";
+    int x = 0;       //this int variable helps while loop, if computer moves a card, of course cannot move another card.
+    String jack = "J";
+
+    for(int i=0; i<52; i++){  
+      if (deck[i].location == 2004){                  //top cards value is in top string now.
+        top = deck[i].value();
+      }
+    }
+
+    if(x == 0){                               //if the top card value equals any card of on computers hand computer gots all.
+    for(int i=0; i<52; i++){  
+        if(deck[i].location == 3){                
+           if(deck[i].value().equals(top)){                     //sets the card the comp throwed 
+            deck[i].assignLocation(5);
+
+
+             for(int j = 0; j<52 ; j++){
+                if(deck[j].location() == 2004){                 //sets top card location
+                    deck[j].assignLocation(5);
+                 }
+                else if(deck[j].location() == 1){               //sets other cards on board location
+                deck[j].assignLocation(5);
+               }
+            }
+
+            System.out.println("computer moved " + deck[i].value() + deck[i].suit() + " and taked cards on board");
+            x++;             //computer moves only one card !
+           }     
+        }
+      }
+    }
+   
+      
+        for(int i=0; i<52 ; i++){
+         if(deck[i].location == 3){
+          if(deck[i].value().equals(jack) && top != ""){
+            if(x == 0){
+            deck[i].assignLocation(5);
+
+            for(int j = 0; j<52 ; j++){
+                if(deck[j].location() == 2004){                 //sets top card location
+                    deck[j].assignLocation(5);
+                 }
+                else if(deck[j].location() == 1){               //sets other cards on board location
+                deck[j].assignLocation(5);
+               }
+            }
+            System.out.println("Computer moves jack and taked all");
+          x = 1;
+        }
+        }
+        }
+        }
+
+           
+            for(int i=0; i<52 ; i++){
+             if(deck[i].location() == 3){
+              while(x == 0){
+
+                for(int j=0; j<52 ; j++){
+                    if(deck[j].location() ==  2004){
+                       deck[j].assignLocation(1);        //previous top card becomes one of the cards on board.
+                    }
+                }
+              
+               deck[i].assignLocation(2004);            //choosed card becomes top.
+    
+                System.out.println("Computer moved " + deck[i].value() + deck[i].suit());
+              x++;
+            }
+            }
+            }
+            
+    x = 0;
+    top = "";
+    return deck;
+}
 
 public static void game(Card[] deck) {      
 
       //--------------VARİABLES, ARRAYS FOR GAME---------------------------------------------
  
-      int score = 0;          
-      int decksonboard = 0;   //how many cards on board
       int deckindex = 0;   //where we are when distributing ?? , if one card out of the deck during game it cannot return.
   
       //---------------------------------------------------------------------------------------
-
-Scanner scn = new Scanner(System.in);
-Random rd = new Random();
 
 System.out.println("WELCOME TO PISTI !!!");
 
@@ -148,11 +382,12 @@ deck = cutter.ComputerCuts();                         //secondly cutting of cour
 for(int i = 0; i<3 ; i++){
     deck[deckindex].assignLocation(1);                     //3 card on board now. 
     deckindex++;
-    decksonboard++;
+   
 }
- deck[deckindex].assignLocation(2004);                 //one card on top !!
- decksonboard++;
+ deck[deckindex].assignLocation(2004);                      //one card on top !!
  deckindex++;
+
+for(int j = 0; j<6 ; j++){                                     //cards distrubiting 6 times !!
 
 for(int i = 0; i<4 ; i++){
     deck[deckindex].assignLocation(2);                     //4 card on players hand.
@@ -164,60 +399,36 @@ for(int i = 0; i<4 ; i++){
     deckindex++;
 }
 
-
-
-
-
-
 view(deck);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+deck = choise(deck);
+view(deck);
+deck = ComputerTurn(deck);
+view(deck);
+deck = choise(deck);
+view(deck);
+deck = ComputerTurn(deck);
+view(deck);
+deck = choise(deck);
+view(deck);
+deck = ComputerTurn(deck);
+view(deck);
+deck = choise(deck);
+view(deck);
+deck = ComputerTurn(deck);
 
 }
 
+for(int i = 0; i<52 ; i++){
+ if(deck[i].location == 1){
+   deck[i].assignLocation(4);             //after the game finished the cards on board added to cards of player have location.
+}
+ if(deck[i].location == 2004){
+    deck[i].assignLocation(4);
+ }
+}
+
+
+}
 
 }
 
